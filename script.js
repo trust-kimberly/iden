bdae4b45ee173f5ccf7ae178587162d4a8c38d9b
Kimbelly Nguyen <Kimchi@Kimbellys-MacBook-Pro.local>
2020-12-14 14:21:20 -0500

    javascript updates

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
+
 $(window).scroll(function(){
     $(".arrow").css("opacity", 1 - $(window).scrollTop() / 250); 
   //250 is fade pixels
   });
+
+
+  $(window).scroll(function() {
+    $("#footer").removeClass("background-color");
+    if($(window).scrollTop() + $(window).height() >= $(document).height() - 400) {
+        //into teal larger footer section
+        $("#footer").addClass("background-color2");
+    } 
+   if($(window).scrollTop() + $(window).height() < $(document).height() - 400) {
+        //above teal larger footer section
+     $("#footer").removeClass("background-color2");   
+     $("#footer").addClass("background-color");
+    } 
+ });
